36be409fff4bf85e3054299b91596513e3e99a4a
Alejomay <dariomay4264@hotmail.com>
2019-10-23 13:18:17 -0500

    DSHOPPING10_Parcial2

---
+++ dshopping/apps/administration/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+# Register your models here.
+from.models import *
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(shopping)
+admin.site.register(Gender)
+admin.site.register(Country)
+admin.site.register(Client)
+
+
